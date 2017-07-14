98a2fa16555446fe4b884a774114a0f431dc1bc1
“刘祥祥” <xiangxiangliu@sf-express.com>
2017-07-14 23:44:20 +0800

    add AndResGuard, ImageLoader

---
+++ library/src/main/java/com/tesla/framework/component/imageloader/IImageLoaderstrategy.java
@@ -0,0 +1,16 @@
+package com.tesla.framework.component.imageloader;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+
+/**
+ * Created by Administrator on 2017/3/20 0020.
+ */
+
+public interface IImageLoaderstrategy {
+    void showImage(@NonNull ImageLoaderOptions options);
+    void cleanMemory(Context context);
+    // 在application的oncreate中初始化
+    void init(Context context);
+}
---
+++ library/src/main/java/com/tesla/framework/component/imageloader/ImageLoaderManager.java
@@ -0,0 +1,53 @@
+package com.tesla.framework.component.imageloader;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+
+
+/**
+ * Created by Administrator on 2017/3/22 0022.
+ */
+
+public class ImageLoaderManager implements IImageLoaderstrategy {
+    private static final ImageLoaderManager INSTANCE=new ImageLoaderManager();
+    private  IImageLoaderstrategy loaderstrategy;
+    private ImageLoaderManager(){
+    }
+    public static ImageLoaderManager getInstance(){
+        return INSTANCE;
+    }
+
+    public  void setImageLoaderStrategy(IImageLoaderstrategy strategy){
+        loaderstrategy=strategy;
+    }
+
+    /*
+     *   可创建默认的Options设置，假如不需要使用ImageView ，
+     *    请自行new一个Imageview传入即可
+     *  内部只需要获取Context
+     */
+    public static ImageLoaderOptions getDefaultOptions(@NonNull View container, @NonNull String url){
+        return new ImageLoaderOptions.Builder(container,url).isCrossFade(true).build();
+    }
+
+    @Override
+    public void showImage(@NonNull ImageLoaderOptions options) {
+        if (loaderstrategy != null) {
+            loaderstrategy.showImage(options);
+        }
+    }
+
+
+    @Override
+    public void cleanMemory(Context context) {
+        loaderstrategy.cleanMemory(context);
+    }
+
+    // 在application的oncreate中初始化
+    @Override
+    public void init(Context context) {
+
+    }
+
+}
